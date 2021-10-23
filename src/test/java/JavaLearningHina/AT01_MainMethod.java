9f2b3590b88aca9c5dae5df507845e77af3978a8
HinaNaazFatima <hina.nazfatima@gmail.com>
2021-10-23 23:22:04 +1100

    SecondFile

--- src/test/java/JavaLearningHina/AT01_MainMethod.java
+++ src/test/java/JavaLearningHina/AT01_MainMethod.java
@@ -1,6 +1,7 @@
 package JavaLearningHina;
 
 public class AT01_MainMethod {
+    //main method
     public static  void main(String[] args)
     {
         System.out.println("This is my 1st class");
